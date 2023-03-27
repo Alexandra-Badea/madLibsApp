31a8500eb19c2991c7ccb70680b91d74f9b36845
Alexandra Badea <alex.alina2998@gmail.com>
2023-03-27 10:48:04 +0300

    Added mad-lib replacement variables to story

---
+++ app.js
@@ -0,0 +1,33 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
+
+let originalStory;
+
+let userInputs = [];
+
+originalStory = `It was a ${userInputs[0]} summer day. ${userInputs[1]} and I were 
+excited to go camping at ${userInputs[2]}. It was my first time going there. I packed my 
+favourite ${userInputs[3]}. On the road we went in our ${userInputs[4]} ${userInputs[5]} 
+car. We were listening to ${userInputs[6]} all the way down. The drive was about 5 hours but 
+it was worth it.`;
+
+console.log(originalStory);
+
+
+
+
+
+
+
+
+
+
+// ORIGINAL STORY
+// let orgStory = `It was a sunny summer day. Ana and I were excited to go camping at the 
+// mountain. It was my first time going there. I packed my favourite tracksuits. On the road 
+// we went in our gray big car. We were listening to Lady Gaga all the way down. The drive was 
+// about 5 hours but it was worth it.`; 
