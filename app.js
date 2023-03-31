dcdd8b04b5ce409bd0bbe0e7f0892121fa358248
Alexandra Badea <alex.alina2998@gmail.com>
2023-03-31 18:55:37 +0300

    revised version

--- app.js
+++ app.js
@@ -2,24 +2,31 @@ function startGame() {
 
   let userInputs = [];
 
-  let numberOfQuestions = 8;
-  let quesitonCounter = 0;
-
   let questionArray = ["adjective", "name", "place", "noun", "adjective", "adjective", "name", "adjective"];
+
+  // Function which validates the inputs
+  function intreaba(intrebare) {
+    let answer = prompt("Enter a/ an " + intrebare);
+    if (answer === "" || answer === null) {
+      alert("Please enter a value");
+      return intreaba(intrebare);
+    }
+    if (!isNaN(answer)) {
+      alert("Please enter a string not a number");
+      return intreaba(intrebare);
+    }
+    return answer;
+  }
 
   // Display prompt question for each element of the questionArray
   for (let i = 0; i < questionArray.length; i++) {
-    let answer = prompt("Enter a/ an " + questionArray[i]);
-
+    let answer = intreaba(questionArray[i]);
     userInputs.push(answer);
-
-    quesitonCounter++;
-    numberOfQuestions--;
 
     // Display in the console: the answer, how many questions answered, how many questions left
     console.log("you answer is: " + userInputs[i]);
-    console.log("You answered: " + quesitonCounter + " questions");
-    console.log("You have: " + numberOfQuestions + " questions left to answer");
+    console.log("You answered: " + (i + 1) + " questions");
+    console.log("You have: " + (questionArray.length - i - 1) + " questions left to answer");
 
     let originalStory = `It was a ${userInputs[0]} summer day. ${userInputs[1]} and I were 
     excited to go camping at ${userInputs[2]}. It was my first time going there. I packed my 
@@ -28,17 +35,13 @@ function startGame() {
     it was ${userInputs[7]} it.`;
 
     // When no quesitons left to answer, alert this and display the story on the screen
-    if (numberOfQuestions == 0) {
+    if (i == (questionArray.length - 1)) {
       alert("You answered all the questions. Let's read your story: ");
 
       let divStory = document.getElementById("story");
       divStory.textContent = originalStory;
-      // console.log(originalStory);
-
     }
   }
-
-
 }
 
 // ORIGINAL STORY
